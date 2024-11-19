5bebb3ce22fdf5cbc792c5128822204aa449eda7
ecekiral <ece.kiral@ozu.edu.tr>
2024-11-19 22:38:32 +0300

    test1

--- CarRentalApp/src/main/java/com/example/CarRentalApp/model/Car.java
+++ CarRentalApp/src/main/java/com/example/CarRentalApp/model/Car.java
@@ -1,8 +1,117 @@
 package com.example.CarRentalApp.model;
 
-public class Car {
-    private String name;
-    private int ID;
-    private String colour;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long barcode;
+    private String licensePlateNumber;
+    private int passengerCapacity;
+    private String brand;
+    private String model;
+    private double mileage;
+    private String transmissionType;
+    private double dailyPrice;
+    private String type;
+    private String status;
+
+    public Car(long barcode, String licensePlateNumber, int passengerCapacity, String brand, String model,
+               double mileage, String transmissionType, double dailyPrice, String type, String status) {
+        this.barcode = barcode;
+        this.licensePlateNumber = licensePlateNumber;
+        this.passengerCapacity = passengerCapacity;
+        this.brand = brand;
+        this.model = model;
+        this.mileage = mileage;
+        this.transmissionType = transmissionType;
+        this.dailyPrice = dailyPrice;
+        this.type = type;
+        this.status = status;
+    }
+
+    public long getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(long barcode) {
+        this.barcode = barcode;
+    }
+
+    public String getLicensePlateNumber() {
+        return licensePlateNumber;
+    }
+
+    public void setLicensePlateNumber(String licensePlateNumber) {
+        this.licensePlateNumber = licensePlateNumber;
+    }
+
+    public int getPassengerCapacity() {
+        return passengerCapacity;
+    }
+
+    public void setPassengerCapacity(int passengerCapacity) {
+        this.passengerCapacity = passengerCapacity;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(double mileage) {
+        this.mileage = mileage;
+    }
+
+    public String getTransmissionType() {
+        return transmissionType;
+    }
+
+    public void setTransmissionType(String transmissionType) {
+        this.transmissionType = transmissionType;
+    }
+
+    public double getDailyPrice() {
+        return dailyPrice;
+    }
+
+    public void setDailyPrice(double dailyPrice) {
+        this.dailyPrice = dailyPrice;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
